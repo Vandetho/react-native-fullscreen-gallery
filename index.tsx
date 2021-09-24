50b1cb5e6671daba983dfa4815d849f978c28382
Vandeth THO <thovandeth@gmail.com>
2021-09-24 15:37:53 +0700

    Correction errors

---
+++ index.tsx
@@ -0,0 +1,4 @@
+export { default as DotGallery } from './src/DotGallery';
+export { default as Gallery } from './src/Gallery';
+export { default as IndicatorGallery } from './src/IndicatorGallery';
+export { default as PreviewGallery } from './src/PreviewGallery';
