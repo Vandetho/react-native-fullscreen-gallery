58f807cf864d68bdf0b0bb8b1868a89b3d7361e6
Vandeth THO <thovandeth@gmail.com>
2021-09-24 19:11:42 +0700

    Update component

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import IndicatorGallery from './IndicatorGallery';
-import PreviewGallery from './PreviewGallery';
 import DotGallery from './DotGallery';
-
-const styles = StyleSheet.create({
-    container: {},
-    indicatorHorizontalContainer: {
-        position: 'absolute',
-        bottom: 25,
-        zIndex: 1000,
-        height: 100,
-        width: Dimensions.get('screen').width,
-    },
-    indicatorVerticalContainer: {
-        position: 'absolute',
-        left: 25,
-        zIndex: 1000,
-        width: 100,
-        height: Dimensions.get('screen').height,
-    },
-});
+import ThumbnailGallery from './ThumbnailGallery';
 
 interface GalleryProps {
     images: any[];
@@ -29,36 +9,12 @@ interface GalleryProps {
 }
 
 const Gallery: React.FunctionComponent<GalleryProps> = ({ images, horizontal = true, indicatorMode = 'thumbnail' }) => {
-    const [activeImage, setActiveImage] = React.useState<number>(0);
-
-    const indicatorStyle = React.useMemo(
-        () => (horizontal ? styles.indicatorHorizontalContainer : styles.indicatorVerticalContainer),
-        [horizontal],
-    );
-
     return React.useMemo(() => {
         if (indicatorMode === 'thumbnail') {
-            return (
-                <View style={styles.container}>
-                    <PreviewGallery
-                        horizontal={horizontal}
-                        images={images}
-                        activeImage={activeImage}
-                        onChangeActiveImage={setActiveImage}
-                    />
-                    <View style={indicatorStyle}>
-                        <IndicatorGallery
-                            horizontal={horizontal}
-                            images={images}
-                            activeImage={activeImage}
-                            onChangeActiveImage={setActiveImage}
-                        />
-                    </View>
-                </View>
-            );
+            return <ThumbnailGallery images={images} horizontal={horizontal} />;
         }
         return <DotGallery images={images} horizontal={horizontal} />;
-    }, [activeImage, horizontal, images, indicatorMode, indicatorStyle]);
+    }, [horizontal, images, indicatorMode]);
 };
 
 export default Gallery;
--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { default as DotGallery } from './DotGallery';
 export { default as Gallery } from './Gallery';
 export { default as IndicatorGallery } from './IndicatorGallery';
 export { default as PreviewGallery } from './PreviewGallery';
+export { default as ThumbnailGallery } from './ThumbnailGallery';
